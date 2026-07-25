08343830f3e6974f5915b308358126298bc31507
Jun1el <latorrevasquezandressebastian@gmail.com>
2026-07-25 11:00:36 -0500

    fix: replace broken react-google-maps with responsive iframe maps link to Puerta 5 UNI

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,50 +1,45 @@
 import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
-
-const APIKEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
-
-const containerStyle = {
-  width: '100%',
-  height: '400px'
-}
-
-const center = {
-  lat: -3.745,
-  lng: -38.523
-}
+import { Box, Link, Flex } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 const Map = () => {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: APIKEY
-  })
-
-  const [, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds(center)
-    map.fitBounds(bounds)
-
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
-  return isLoaded
-    ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
+  return (
+    <Flex flexDir='column' gap={3} width='full'>
+      <Box
+        width='full'
+        height='400px'
+        borderRadius='xl'
+        overflow='hidden'
+        boxShadow='md'
+        borderWidth='1px'
+        borderColor='smoke.300'
       >
-        <></>
-      </GoogleMap>
-      )
-    : <></>
+        <iframe
+          src='https://maps.google.com/maps?q=Puerta%205,%20Universidad%20Nacional%20de%20Ingenieria,%20Rimac,%20Lima&t=&z=16&ie=UTF8&iwloc=&output=embed'
+          width='100%'
+          height='100%'
+          style={{ border: 0 }}
+          allowFullScreen=''
+          loading='lazy'
+          referrerPolicy='no-referrer-when-downgrade'
+        />
+      </Box>
+      <Link
+        href='https://maps.app.goo.gl/2Q8Y2f7jbNoh3gtS6'
+        isExternal
+        color='brand.100'
+        fontWeight='semibold'
+        fontSize='sm'
+        display='flex'
+        alignItems='center'
+        gap={1}
+        width='fit-content'
+        _hover={{ textDecoration: 'underline', color: 'brand.200' }}
+      >
+        Ver ubicación en Google Maps <ExternalLinkIcon mx='2px' />
+      </Link>
+    </Flex>
+  )
 }
 
 export default Map
