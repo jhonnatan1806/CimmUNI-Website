7982f0228f26082898fbb391178b69e74bf42f33
Jun1el <latorrevasquezandressebastian@gmail.com>
2026-08-08 17:28:36 -0500

    feat: reactivate comites route, implement tabs layout, and update scientific and organizing committee members

--- src/pages/comites.js
+++ src/pages/comites.js
@@ -3,7 +3,12 @@ import {
   Flex,
   Grid,
   GridItem,
-  Heading
+  Heading,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel
 } from '@chakra-ui/react'
 import CardTeam from '@/components/CardTeam'
 import comites from '@/data/comites.json'
@@ -11,39 +16,54 @@ import comites from '@/data/comites.json'
 export default function Comites({ cientifico, organizador }) {
   return (
     <Layout>
-      <Flex flexDir='column' gap={4} mb={8}>
-        <Heading>Comite Científico</Heading>
-        <Grid
-          templateColumns={[
-            'repeat(2, 1fr)',
-            'repeat(3, 1fr)',
-            'repeat(4, 1fr)'
-          ]}
-          gap={4}
-        >
-          {cientifico.map((item, index) => (
-            <GridItem key={index}>
-              <CardTeam team={item} />
-            </GridItem>
-          ))}
-        </Grid>
-      </Flex>
-      <Flex flexDir='column' gap={4} mb={8}>
-        <Heading>Comite Organizador</Heading>
-        <Grid
-          templateColumns={[
-            'repeat(2, 1fr)',
-            'repeat(3, 1fr)',
-            'repeat(4, 1fr)'
-          ]}
-          gap={4}
-        >
-          {organizador.map((item, index) => (
-            <GridItem key={index}>
-              <CardTeam team={item} />
-            </GridItem>
-          ))}
-        </Grid>
+      <Flex flexDir='column' gap={6} mb={8} py={4}>
+        <Heading as='h2' size='lg' color='brand.100' textAlign='center'>
+          Comités CIMM-UNI 2026
+        </Heading>
+
+        <Tabs isFitted variant='enclosed' colorScheme='red' width='full'>
+          <TabList mb='1em'>
+            <Tab fontWeight='bold'>Comité Científico</Tab>
+            <Tab fontWeight='bold'>Comité Organizador</Tab>
+          </TabList>
+          <TabPanels>
+            {/* Comité Científico */}
+            <TabPanel>
+              <Grid
+                templateColumns={[
+                  'repeat(2, 1fr)',
+                  'repeat(3, 1fr)',
+                  'repeat(4, 1fr)'
+                ]}
+                gap={4}
+              >
+                {cientifico.map((item, index) => (
+                  <GridItem key={index}>
+                    <CardTeam team={item} />
+                  </GridItem>
+                ))}
+              </Grid>
+            </TabPanel>
+
+            {/* Comité Organizador */}
+            <TabPanel>
+              <Grid
+                templateColumns={[
+                  'repeat(2, 1fr)',
+                  'repeat(3, 1fr)',
+                  'repeat(4, 1fr)'
+                ]}
+                gap={4}
+              >
+                {organizador.map((item, index) => (
+                  <GridItem key={index}>
+                    <CardTeam team={item} />
+                  </GridItem>
+                ))}
+              </Grid>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Flex>
     </Layout>
   )
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 const routes = [
   { name: 'Inicio', path: '/' },
   { name: 'Expositores', path: '/expositores' },
-  // { name: 'Comités', path: '/comites' },
+  { name: 'Comités', path: '/comites' },
   // { name: 'Cronograma', path: '/cronograma' },
   { name: 'Registro', path: '/registro' },
   { name: 'Contacto', path: '/contacto' }
